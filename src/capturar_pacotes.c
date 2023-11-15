660eaf9a669239b54abccee8582bf2819e640a3b
elieltondias <elieltondiasramosdesouza@gmail.com>
2023-11-15 11:02:41 -0300

    verifica root

--- src/capturar_pacotes.c
+++ src/capturar_pacotes.c
@@ -6,47 +6,55 @@
 #include <sys/types.h>
 
 
-// Cria uma animação curta para a execução
+//cria uma animação curta para a execução
 void animacao_de_execucao(){
     char animacaoDeCaracteres[] = {'|', '/', '-', '\\'};
 
     int i;
-    for (i = 0; i < 200; i++) {
-        printf("\r%c Voce tem 30 segundos caso queira cancelar antes de iniciar... (Ctrl + c para cancelar!) %c", animacaoDeCaracteres[i % 4], animacaoDeCaracteres[i % 4]);
+    for (i = 0; i < 100; i++) {
+        printf("\r%c Captura sera iniciada... (Ctrl + c para cancelar!) %c", animacaoDeCaracteres[i % 4], animacaoDeCaracteres[i % 4]);
         fflush(stdout);
-        usleep(100000);  // pausa por 100 milissegundos (100000 microssegundos)
+        usleep(10000);  //pausa por 10 milissegundos
     }
     printf("\n");
 }
 
 int main(){
+
+  // 1. VERIFICA SE ESTA SENDO EXECUTADO COMO ROOT
+
+  if (geteuid() != 0) {
+      printf("Este programa precisa de acesso root. Por favor, execute como root.\n");
+      exit(EXIT_FAILURE);
+  }
+  //se estiver sendo executado como root, continue
+  printf("Operações privilegiadas podem ser realizadas aqui.\n\n");
+
+  
+  //comando "clear" para limpar o terminal
+  printf("\033[2J\033[H"); // a sequência \033[2J limpa a tela, e \033[H move o cursor para a posição superior esquerda
+
   
   pcap_t *secaoDeCaptura;
   char cacheDeErros[PCAP_ERRBUF_SIZE];
   struct pcap_pkthdr cabecalhoInfo;
   const uint8_t *pacotes;
 
-  //abre a interface de rede wireless
+  // 2. ABRE A INTERFACE DE REDE WIRELESS
   
   int tamanho_maximo_do_pacote;
-  printf("QUAL O TAMANHO MAXIMO DO PACOTE(em bytes)?\n");
+  printf("Tamanho maximo do pacote(em bytes)?\n");
   scanf("%i", &tamanho_maximo_do_pacote);
-
-  if (tamanho_maximo_do_pacote == NULL){
-    int tamanho_maximo_do_pacote = 1000000;
-  }else if(tamanho_maximo_do_pacote == 0){
-   int tamanho_maximo_do_pacote = 1000000;
-  }
+  if(tamanho_maximo_do_pacote = 0){
+    tamanho_maximo_do_pacote = 1000000;
+  }else{}
 
   int tempo_limite;
-  printf("QUAL O TEMPO LIMITE PARA CAPTURAR CADA PACOTE? (em ms)\n");
+  printf("Tempo limite para cada pacote? (em ms)\n");
   scanf("%i", &tempo_limite);
-
-  if (tempo_limite == NULL){
-    int tempo_limite = 1000 ;
-  }else if(tempo_limite == 0){
+  if(tempo_limite = 0){
     int tempo_limite = 1000;
-  }
+  }else{}
   
   secaoDeCaptura = pcap_open_live("wlan0", tamanho_maximo_do_pacote, 1, tempo_limite, cacheDeErros); 
   // secaoDeCaptura = pcap_open_live("interface de rede", tamanho do pacote em bytes, 1 para ativar o modo promiscuo, tempo limite para capturar o pacote, eliminaar cache e erros)
@@ -60,28 +68,33 @@ int main(){
   */
 
   if (secaoDeCaptura == NULL) {
-    printf("Erro ao abrir a interface de rede: %s\n", cacheDeErros);
+    printf("Erro ao abrir a interface de rede: %s\n\n", cacheDeErros);
     return 1;
   }
 
-  //cria a pasta para guardar os pacotes
+  // 3. CRIA PASTA PARA GUARDAR OS PACOTES
+
   char *pasta = "Pacotes";
 
   int status = mkdir(pasta, 0777);
 
   //verifica se a pasta foi criada com sucesso
   if (status == 0) {
-    printf("Pasta '%s' criada com sucesso.\n", pasta);
+    printf("Pasta '%s' criada com sucesso.\n\n", pasta);
   } else {
-    printf("Erro ao criar a pasta '%s'.\n", pasta);
+    printf("Erro ao criar a pasta '%s'.\n\n", pasta);
   }
 
-  //captura os pacotes
-  printf("A captura de pacotes sera iniciada iniciada em 20 segundos!\n\n");
-  animacao_de_execucao();
-  printf("CAPTURA EM EXECUÇÃO!\n");
+  // 4. CAPTURA OS PACOTES
 
-  // Loop que captura os pacotes continuamete
+  //animação de espera
+  animacao_de_execucao();
+  //limpa o terminal
+  printf("\033[2J\033[H");
+
+  printf("CAPTURA EM EXECUÇÃO! (Ctrl + c para cancelar!)\n");
+
+  //loop que captura os pacotes continuamete
   while(1){
     pacotes = pcap_next(secaoDeCaptura, &cabecalhoInfo);
     if (pacotes == NULL){
@@ -96,11 +109,11 @@ int main(){
     A importância de um arquivo pcap reside na sua capacidade de facilitar a identificação de diversas anomalias nas operações da rede.
     */
 
-    // Cria um nome de arquivo com base no carimbo de data/hora
+    //cria um nome de arquivo com base no carimbo de data/hora
     char filename[255];
     sprintf(filename, "Pacotes/%d.pcap", cabecalhoInfo.ts.tv_sec);
 
-    // Abre um arquivo de despejo para escrever
+    //abre um arquivo de despejo para escrever
     pcap_dumper_t *pcapDumper = pcap_dump_open(secaoDeCaptura, filename);
 
     if (pcapDumper == NULL){
@@ -108,10 +121,10 @@ int main(){
       return 1;
     }
 
-    // Grava o pacote no arquivo de despejo
+    //grava o pacote no arquivo de despejo
     pcap_dump((unsigned char *) pcapDumper, &cabecalhoInfo, pacotes);
   
-    // Fecha o arquivo de despejo
+    //fecha o arquivo de despejo
     pcap_dump_close(pcapDumper);
   }
 
